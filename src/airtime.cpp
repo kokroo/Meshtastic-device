fda2ff9587d3ede50fd65e52dbc2f1ee18df5e0f
Shiv Kokroo <kokroo@users.noreply.github.com>
2023-12-30 02:25:19 +0100

    Ignore airtime

--- src/airtime.cpp
+++ src/airtime.cpp
@@ -134,15 +134,16 @@ bool AirTime::isTxAllowedChannelUtil(bool polite)
 
 bool AirTime::isTxAllowedAirUtil()
 {
-    if (!config.lora.override_duty_cycle && myRegion->dutyCycle < 100) {
-        if (utilizationTXPercent() < myRegion->dutyCycle * polite_duty_cycle_percent / 100) {
-            return true;
-        } else {
-            LOG_WARN("Tx air utilization is >%f percent. Skipping this opportunity to send.\n",
-                     myRegion->dutyCycle * polite_duty_cycle_percent / 100);
-            return false;
-        }
-    }
+    // if (!config.lora.override_duty_cycle && myRegion->dutyCycle < 100) {
+    //     if (utilizationTXPercent() < myRegion->dutyCycle * polite_duty_cycle_percent / 100) {
+    //         return true;
+    //     } else {
+    //         LOG_WARN("Tx air utilization is >%f percent. Skipping this opportunity to send.\n",
+    //                  myRegion->dutyCycle * polite_duty_cycle_percent / 100);
+    //         return false;
+    //     }
+    // }
+    // Ignore airtime
     return true;
 }
 
